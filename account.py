9306e43642ec440958dd0f265bff6877c274aee6
geuresti <54899735+geuresti@users.noreply.github.com>
2020-12-19 11:38:55 -0600

    Update account.py
    added email and username checks

--- account.py
+++ account.py
@@ -1,32 +1,63 @@
 import cryptography
 from cryptography.fernet import Fernet
 
+# the Account class allows for the creationg of accounts, storing account
+# information to a file, assigning an id, and password encrytion/decryption
 class Account:
-
-    id = 0
+    # ID = 0
+    # USERNAME = 1
+    # PASSWORD = 2
+    # EMAIL = 3
+    # FILE = 4
 
     def __init__(self, username, password, email):
         self.username = username
         self.email = email
-        #self.storeInfo(password)
-        self.id = Account.idAccount()
+        self.id = Account.id_account()
+        self.workout_file = self.username + str(self.id) + ".txt"
 
-    @classmethod
-    def idAccount(cls):
-        cls.id += 1
-        return cls.id
+        # UNCOMMENT LINES BELOW WHEN ADDING NEW ACCOUNTS
 
-    def storeInfo(self, password):
+        file = open(self.workout_file, "w")
+        file.write("weight:,calories bulk:,calories cut:,calories maintenance:,")
+        file.close()
+
+        self.store_info(password)
+
+    # check if there is already an account with the inputted username / email
+    def has_user_or_email(info):
+        file = open("account_data.txt", "r")
+
+        if "@" in info:
+            index = 3
+        else:
+            index = 1
+            
+        for line in file:
+            line = line.split("|")
+            if line[index] == info:
+                return True
+        return False
+        
+    # assigns an Account an id based on their line number, starts at 1
+    def id_account():
+        id = 1
+        f = open("account_data.txt", "r")
+        for line in f:
+            id += 1
+        return id
+
+    # writes the account information to a text file. password is passed as
+    # an argument so there isn't a password directly associated with
+    # an Account object instance (the unencrypted password is unable to be looked up)
+    def store_info(self, password):
         f = open("account_data.txt", "a")
-        pswrd = Account.encryptPassword(password)
-        print(f"{self.username}|{pswrd}|{self.email}", file=f)
+        pswrd = Account.encrypt_password(password)
+        print(f"{self.id}|{self.username}|{pswrd}|{self.email}|{self.workout_file}", file=f)
         f.close()
-
-#    key = Fernet.generate_key()
-#    file = open('encrypt_key.key', 'wb')
-#    file.write(key)
-#    file.close()
-    def encryptPassword(msg):
+        
+    # uses the encrypt_key.key file to encrypt a String argument
+    def encrypt_password(msg):
         file = open('encrypt_key.key', 'rb')
         key = file.read()
         file.close()
@@ -37,7 +68,8 @@ class Account:
 
         return encrypted_message
 
-    def decryptPassword(msg):
+    # takes an encrypted string and returns the its decrypted value
+    def decrypt_password(msg):
         file = open('encrypt_key.key', 'rb')
         key = file.read()
         file.close()
@@ -47,28 +79,38 @@ class Account:
 
         return decrypted_password
 
-    # refactor
+    def display_profile(workout_file):
+        file = open(workout_file, "r")
+        info = file.readline().split(",")
+        for data in info:
+            print(data)
+
+    # *refactor*
+    # compares the username and password (as a list) to information in the
+    # account data text file, returns true if there is a match
     @classmethod
-    def checkInfo(cls, info):
+    def check_info(cls, info):
         f = open("account_data.txt", "r")
         for line in f:
             data = line.split("|")
-            data[2] = data[2].strip("\n")
+            # this line orignally had data[2]
+            data[4] = data[4].strip("\n")
 
-            if data[0] == info[0]:
-                stuff = data[1]
+            if data[1] == info[0]:
+                stuff = data[2]
                 stuff = stuff[2:len(stuff)-1]
                 stuff = stuff.encode()
-                stuff = Account.decryptPassword(stuff)
+                stuff = Account.decrypt_password(stuff)
 
                 info[1] = info[1].encode()
 
                 if stuff == info[1]:
                     f.close()
-                    print("Logged in")
-                    return True
+                    print("-Logged in-")
+                   # return True
+                    return data
 
-        print("Log in failed")
+        print("-Log in failed-")
         f.close()
         return False
 
@@ -76,12 +118,5 @@ class Account:
         return f"Username: {self.username}\nEmail: {self.email}"
 
 if __name__ == '__main__':
-    log_in_attempt = ["test_dummy", "redLines123"]
-    Account.checkInfo(log_in_attempt)
-    user = Account("new_person", "new_password", "new_email@something.com")
-    print(type(user))  # store accounts to file as Account objects for logging in?
-    print(user.id)
-    print(Account.id)
-    user_two = Account("new_person_two", "new_pass", "new_email@xd.com")
-    print(user_two.id)
-    print(Account.id)
+    
+    pass
